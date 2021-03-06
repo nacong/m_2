c2ebc0ef5689cf0aaa4c2746824e5e7a43342e47
nacong <nabinyang37@gmail.com>
2021-03-06 14:26:47 +0900

    first

---
+++ src/components/box.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Box = () => (
+    <div className="artist">
+        <div id="mdtibox">
+            <p id="title">제목입니다열글자글자</p>
+            <p id="artist_2">·아티스트·</p>
+            <img alt="" id="img" src="https://images.unsplash.com/photo-1604922824961-87cefb2e4b07?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8MSUzQTF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60"></img>
+            <div id="tags">
+                <p id="tag">하늘색</p>
+                <p id="tag">초록색</p>
+                <p id="tag">실사</p>
+                <p id="tag">수평구도</p>
+                <p id="tag">풍경</p>
+                <p id="tag">아무태그</p>
+            </div>
+        </div>
+    </div>
+)
+
+export default Box;
